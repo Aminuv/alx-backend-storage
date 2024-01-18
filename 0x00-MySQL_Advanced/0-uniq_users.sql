25e0f266e01b28257b2de7f75752311dda03d075
Aminuv <mohamed.elmarbouh07@gmail.com>
2024-01-18 19:59:33 +0000

    fix tasks 00

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,4 @@
--- Thhe script that creates 'table users' with follwing fields.
+-- The script that creates 'table users' with follwing fields.
 -- id, email, name
 CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
